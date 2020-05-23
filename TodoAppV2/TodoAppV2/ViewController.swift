e762a039dfdd24f7f852f27cace1c622354453e1
EB21 <ebaraja1@mail.sfsu.edu>
2020-05-22 19:15:02 -0700

    Adding fresh project to github since I was getting issues with database on old commits. Doing them one by one to resolve issue.

---
+++ TodoAppV2/TodoAppV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoAppV2
+//
+//  Created by Edward Barajas on 5/22/20.
+//  Copyright © 2020 Edward Barajas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
